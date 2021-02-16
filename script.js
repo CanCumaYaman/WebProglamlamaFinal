83aacbd89650a95111c0e9f64aed0bf3c179c2b0
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-02-16 15:04:53 +0300

    update

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
+
+    document.getElementById("total").innerHTML=Number(document.getElementById("total").innerHTML)- Number($(this).parent("li").find(".price").text());
   }
 }
 
@@ -27,17 +29,31 @@ list.addEventListener('click', function(ev) {
 }, false);
 
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("somun").innerHTML;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
+function newElement(value) {
  
-  if (inputValue.equals($("#etext"))) {
-    var deger=parseInt(document.getElementById("eadet"));
-    document.getElementById("eadet").innerHTML=deger+1;
-  } 
+  if(value==document.getElementById("etext").innerHTML){
+    var deger=Number(document.getElementById("eadet").innerHTML);
+    deger=deger+1;
+    document.getElementById("eadet").innerHTML=deger;
+    document.getElementById("efiyat").innerHTML=deger*2;
+  }
+  else
+    if(value==document.getElementById("dtext").innerHTML){
+    var deger=Number(document.getElementById("dadet").innerHTML);
+    deger=deger+1;
+    document.getElementById("dadet").innerHTML=deger;
+    document.getElementById("dfiyat").innerHTML=deger*6;
+  }else 
+  if(value==document.getElementById("ctext").innerHTML){
+    var deger=Number(document.getElementById("cadet").innerHTML);
+    deger=deger+1;
+    document.getElementById("cadet").innerHTML=deger;
+    document.getElementById("cfiyat").innerHTML=deger*5;
+  }
+    document.getElementById("total").innerHTML=Number(document.getElementById("efiyat").innerHTML)+Number(document.getElementById("dfiyat").innerHTML)+Number(document.getElementById("cfiyat").innerHTML)-1;
+  }
   document.getElementById("myInput").value = "";
+  
  
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -51,5 +67,5 @@ function newElement() {
       div.style.display = "none";
     }
   }
-}
+
 // <span onclick="newElement()" class="addBtn">Add</span>
